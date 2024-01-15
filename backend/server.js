38cce395c635ed4fdcf41513942879a1c619c429
niyas <niyaskn01@gmail.com>
2024-01-15 11:48:33 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,16 @@ const express=require('express')
 const cors=require('cors')
 require('dotenv').config()
 const db=require('./config/connect')
-const userRouter=require('./routes/userRoutes')
+const userRouter=require('./routes/userRoutes') 
 const app=express()
 
 app.use(express.json())
 app.use(cors())
 db()
-app.use('/user',userRouter)
+app.use('/user',userRouter) 
 
 const port=process.env.PORT || 8080
-app.listen(port,()=>{
+app.listen(port,()=>{ 
   console.log('server is running at port',port);
 })
 
--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance=axios.create({
-  baseURL:"http://localhost:8080"
+  baseURL:"https://mui-backend-by3k.onrender.com"
 })
 
 export default instance
