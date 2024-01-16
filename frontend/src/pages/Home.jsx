16d9cc5cb03c72e10aeac7f3c0ab73e3e06892cf
niyas <niyaskn01@gmail.com>
2024-01-16 13:35:29 +0530

    first commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,10 @@ import Layout from '../components/Layout'
 import { Box, Paper, Typography } from '@mui/material'
 
 function Home() {
-
   return (
     <Layout>
       <Box sx={{marginTop:10}}>
-        <Paper sx={{bgcolor:'#bcceeb',margin:'10px auto',width:400,height:300}}>
+        <Paper elevation={9} sx={{bgcolor:'#bcceeb',margin:'10px auto',width:400,height:300}}>
           <Typography textAlign='center'>Home page</Typography>
         </Paper>
       </Box>
--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const cors=require('cors')
 require('dotenv').config()
 const db=require('./config/connect')
 const userRouter=require('./routes/userRoutes') 
+const path=require('path')
 const app=express()
 
-app.use(express.json())
+app.use(express.static(path.join(__dirname,'frontend','build')))
+app.use(express.json()) 
 app.use(cors())
 db()
-app.use('/user',userRouter) 
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname,'frontend','build','index.html'))
+})
+app.use('/user',userRouter)   
 
 const port=process.env.PORT || 8080
 app.listen(port,()=>{ 
